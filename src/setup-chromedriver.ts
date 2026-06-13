eed771882822354e72c673b8d45aed6fe7441e5d
actionchain <actionchain@chainguard.dev>
2026-06-13 22:37:20 +0000

    Update nanasess/setup-chromedriver@v2.4.0

---
+++ src/setup-chromedriver.ts
@@ -0,0 +1,51 @@
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as path from "path";
+import * as os from "os";
+
+async function run() {
+  try {
+    console.log(`##setup chromedriver`);
+    const version = core.getInput("chromedriver-version", { required: false });
+    const chromeapp = core.getInput("chromeapp", { required: false });
+    const plat = process.platform;
+    let arch = "linux";
+    switch (plat) {
+      case "win32":
+        arch = plat;
+        break;
+      case "darwin":
+        // Check if running on ARM64 macOS (Apple Silicon)
+        if (os.arch() === "arm64") {
+          arch = "mac-arm64";
+        } else {
+          arch = "mac64";
+        }
+        break;
+      default:
+      case "linux":
+        arch = "linux64";
+    }
+    if (arch == "win32") {
+      await exec.exec(
+        "powershell -File " + path.join(__dirname, "../lib", "setup-chromedriver.ps1 "), [
+        version,
+        chromeapp
+      ]);
+    } else {
+      await exec.exec(path.join(__dirname, "../lib", "setup-chromedriver.sh"), [
+        version,
+        arch,
+        chromeapp,
+      ]);
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    } else {
+      throw error;
+    }
+  }
+}
+
+run();
